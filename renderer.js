23c68934fe801fc66348ddce772ae0ec888205b0
Chenyao2333 <louchenyao@gmail.com>
2017-06-27 22:52:41 +0800

    openzrt

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+const BrowserWindow = require('electron').remote.BrowserWindow
+const path = require('path')
+
+const newWindowBtn = document.getElementById('openzrt')
+
+newWindowBtn.addEventListener('click', function (event) {
+  let win = new BrowserWindow({ width: 400, height: 320 });
+  win.on('close', function () { win = null });
+  win.loadURL("https://zrt.io");
+  win.show()
+})
