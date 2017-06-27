ea7858ef185373a43f9fc1c8845ee6a08368b090
zrt <me@zrt.io>
2017-06-27 23:00:09 +0800

    a

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const BrowserWindow = require('electron').BrowserView
+const path = require('path')
+
+const newWindowBtn = document.getElementById('testpage')
+
+newWindowBtn.addEventListener('click',function(event){
+    const modalPath = path.join('file://',__dirname,'testpage.html')
+    let win = new BrowserWindow({width: 800,height:600})
+    win.on('close', function(){
+    win = null
+    })
+    win.loadURL(modalPath)
+    win.show()
+})
