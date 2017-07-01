3acaf6b162bbc92ae4df71b877815b23d1ade7ad
Ruotian <zrt@Ruotians-MacBook-Pro.local>
2017-07-01 11:30:40 +0800

    aa

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'myai.html'),
+    pathname: path.join(__dirname, 'home.html'),
     protocol: 'file:',
     slashes: true
   }))
